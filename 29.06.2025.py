2c51fecb538482f4f1deff0016df34bb40abdf7e
cemcansin <cemcalayir3@gmail.com>
2025-06-29 17:21:41 +0300

    while loops 01

---
+++ 29.06.2025.py
@@ -0,0 +1,16 @@
+# take the initial cell population as input
+cells = int(input())
+
+# take the number of days as input
+days = int(input())
+
+# initialize the day counter
+counter = 1
+
+#complete the while loop
+while counter <= days:
+ 
+  cells = int(cells) * 2
+  # Daily message
+  print("Day " + str(counter) + ": " +str(cells))
+  counter = int(counter) + 1
